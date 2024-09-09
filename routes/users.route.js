3848421c2121c3cc07b9be5102533c698602ce61
sohilf88 <48861940+sohilf88@users.noreply.github.com>
2024-09-09 16:16:21 +0530

    Backend Done

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,15 +1,11 @@
 const router = require("express").Router();
 
 const {
-  updateLoginUserController
+ 
 } = require("../controllers/user.controller");
 const { updateUserProfile } = require("../controllers/user/profile/userProfileData");
 const { userProfile } = require("../controllers/user/profile/userProfile");
 const { verifyJWT } = require("../middlewares/verifyJwt");
-
-
-
-
 
 // user profile
 
@@ -17,8 +13,7 @@ router.get("/profile",verifyJWT, userProfile)
 
 router.patch("/profile",verifyJWT, updateUserProfile)
 
-// update Login user
-// router.patch("/loginUser", updateLoginUserController);
+
 
 module.exports = router
 

41c856e024fb1d8dd946520fd574580c285394de
sohilf88 <48861940+sohilf88@users.noreply.github.com>
2024-09-13 00:59:35 +0530

    File Vaidation and check uploaded extension done

--- controllers/camera/bulk-import.js
+++ controllers/camera/bulk-import.js
@@ -3,36 +3,82 @@ const cameraSchemaModel = require("../../models/camera.model");
 const { ApplicationError } = require("../../middlewares/errorHandler");
 const csv = require("csvtojson")
 const fs = require("fs")
+const Joi = require("joi");
+const path = require('path')
 const bulkCameraImports = asyncHandler(async (req, res, next) => {
+
     // console.log(req.file)
+    // console.log(path.extname(req.file.filename)!==".csv")
+    if (!req.file || path.extname(req.file.filename)!==".csv") {
+       
+        
+           
 
-    if (!req.file) {
-        return new ApplicationError("File was not Found", 400)
+
+        return res.status(400).json({ suceess: false, message: "file was not found or invalid csv format file" })
+       
     }
+       
+    const filePath = req.file.path   
     let bulkCameras = []
+    
+    
 
-    const path = req.file.path
+    //do the stuff for validation
+    const schema = Joi.object({
+        name: Joi.string().required(),
+        district: Joi.string().required(),
+        taluka: Joi.string().required(),
+        city: Joi.string().required(),
+        area: Joi.string().required(),
+        url: Joi.string().required(),
+        isActive: Joi.allow(),
+    });
+
     try {
         // converting into json
-        csv().fromFile(req.file.path).then(async (response) => {
-           
-            // console.log("there")
-            // return res.status(400).json({ success: false, message: "invalid file type" })
-            for (let item = 0; item < response.length; item++) {
+        csv().fromFile(filePath).then(async (response) => {
+            // validating data from csv
+            let errors = [];
+            response.forEach(obj => {
+                // console.log(obj)
+                let value = schema.validate(obj);
 
-            //     //  pushing into array of camera object
+                if (value.error) {
+                    value.error.details.forEach(err => {
+                        errors.push(err.message);
+                    });
+                }
 
-                bulkCameras.push({
-                    name: response[item].name,
-                    district: response[item].district,
-                    taluka: response[item].taluka,
-                    area: response[item].area,
-                    city: response[item].city,
-                    url: response[item].url,
-                    userId: req.user._id
-                })
+            });
+            // console.log(errors.length)
+            if (errors.length > 0) {
+                return res.status(400).json({ sucess: false, message: "invalid data in csv file" })
+                // console.log('validation error from the CSV file');
+                // return new ApplicationError("invalid File uploaded !!!", 400)
+
+            } else {
+                req.response = response
+                // Validation done for CSV file
+                for (let item = 0; item < response.length; item++) {
+
+                    //    pushing data into array of camera object
+
+                    bulkCameras.push({
+                        name: response[item].name,
+                        district: response[item].district,
+                        taluka: response[item].taluka,
+                        area: response[item].area,
+                        city: response[item].city,
+                        url: response[item].url,
+                        userId: req.user._id
+                    })
+                }
+
             }
-            // if(!bulkCameras.length || bulkCameras.name===undefined) return new ApplicationError("oops,Something went wrong123", 400)
+
+
+
             const bulkImports = await cameraSchemaModel.insertMany(bulkCameras)
             // console.log(bulkImports)
             if (!bulkImports) {
@@ -41,9 +87,9 @@ const bulkCameraImports = asyncHandler(async (req, res, next) => {
             }
 
             // delete the uploaded file after data uploaded into DB
-            fs.unlink(path, (error) => {
+            fs.unlink(filePath, (error) => {
                 if (error) {
-                    console.log(error)
+                    // console.log(error)
                     return
                 }
 

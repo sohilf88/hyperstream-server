4fe76790d2912e252f1d1aca5dc722afb5cc7211
sohilf88 <48861940+sohilf88@users.noreply.github.com>
2024-09-15 20:46:43 +0530

    allowed origin added

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     "http://localhost:3000",
-    "https://nextjs14-hyper-stream2024-cdprtw8cf-sohilf88s-projects.vercel.app",
+    "https://nextjs14-hyper-stream2024.vercel.app",
+    "https://nextjs14-hyper-stream2024.vercel.app/auth/login",
     "https://www/hyperstream.com",
     "https://hyperstream.com"
 ]

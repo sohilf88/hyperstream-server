c4a8f28e6317070f96ffbfa585ba83532ec81351
sohilf88 <48861940+sohilf88@users.noreply.github.com>
2024-09-16 08:31:25 +0530

    Cookies none to None

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -47,7 +47,7 @@ const loginController = asyncHandler(async (req, res, next) => {
     // create cookie with refresh token
     return res.cookie('jwtRe', refreshToken, {
         httpOnly: true, //accessible only via browser
-        sameSite: "none",// cross-site cookie
+        sameSite: "None",// cross-site cookie
         secure: true,//https only,need to change to true later
         maxAge: 60 * 60 * 24 * 1000
 
@@ -57,7 +57,7 @@ const loginController = asyncHandler(async (req, res, next) => {
     })
         .cookie('jwtAccess', accesstoken, {
             httpOnly: true, //accessible only via browser
-            sameSite: "none",// cross-site cookie
+            sameSite: "None",// cross-site cookie
             secure: true,//https only,need to change to true later
             maxAge: 15 * 60 * 1000 // 15 minutes
             // expiresIn: 10000 // 48 hours,
